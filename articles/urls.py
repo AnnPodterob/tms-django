17c3e4a9fb23e21fbdfa3944fe0306686ea0e2a6
Ann Podterob <annapodterob@yandex.by>
2023-04-17 08:21:50 +0300

    Initial commit

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:article_id>/', views.detail, name='detail'),
+    path('<int:article_id>/like', views.like, name='like'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpRequest, Http404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpRequest
+from .models import Articles
 
-def index(request):
-    return HttpResponse("Hello Django!")
+
+def index(request: HttpRequest):
+    articles = Articles.objects
+    context = {'articles_list': articles}
+    return render(request, 'articles/index.html', context)
+
+def detail(request, article_id: int):
+    article = get_object_or_404(Articles, id=article_id)
+    context = {'article': article}
+    return render(request, 'articles/detail.html', context)
+
+
+def like(request, article_id: int):
+    article = get_object_or_404(Articles, id=article_id)
+    article_id = request.POST['article_id']
+    article.likes += 1
+    article.save()
+    return redirect('articles:detail', article_id)
